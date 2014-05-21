3600788143d576a4152e08cf4ae27eef1e50a96a
Eric Suh <ericsuh@group360.com>
2014-05-21 13:21:10 -0500

    Structure for all pages.

--- assets/javascripts/modules/transitions.js
+++ assets/javascripts/modules/transitions.js
@@ -8,6 +8,30 @@
                 $('.landing').fadeOut('slow');
                 $('.start').delay( 800 ).fadeIn( 'slow' );
             });
+
+            $('#start-quiz-btn').on('click', function(){
+                event.preventDefault();
+                $('.start').fadeOut('slow');
+                $('.questions').delay( 800 ).fadeIn( 'slow' );
+            });
+
+            $('#next-quiz-btn').on('click', function(){
+                event.preventDefault();
+                $('.questions').fadeOut('slow');
+                $('.end').delay( 800 ).fadeIn( 'slow' );
+            });
+
+            $('#end-quiz-btn').on('click', function(){
+                event.preventDefault();
+                $('.end').fadeOut('slow');
+                $('.adventure').delay( 800 ).fadeIn( 'slow' );
+            });
+
+            $('#adventure-quiz-btn').on('click', function(){
+                event.preventDefault();
+                $('.adventure').fadeOut('slow');
+                $('.social').delay( 800 ).fadeIn( 'slow' );
+            });
         };
 
         return {
