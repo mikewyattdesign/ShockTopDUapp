44691eb90aa22751512a52dee012edb0fefa10e4
Michael Wyatt <michaelwyatt@group360.com>
2014-07-28 16:40:11 -0500

    Alter age gate controller to actually let you advance if Facebook says you're old enough.

--- assets/javascripts/angular/controllers/gate-controller.js
+++ assets/javascripts/angular/controllers/gate-controller.js
@@ -3,19 +3,20 @@ angular.module('unfiltered')
         ['$location', '$scope', '$rootScope',
         function ($location, $scope, $rootScope) {
 
-            $rootScope.oldEnough = $("meta[name='old_enough']").attr('content') === "true";
-
-            // Set new entry dateOfBirth if old enough
-            if ($rootScope.oldEnough) {
-                    $scope.newEntry = {};
-                    $scope.dateOfBirth = '07/28/1986';
-            }
             this.date = {};
             this.months = [];
             this.days = [];
             this.years = [];
             this.legalDate = moment().subtract('years', 21);
 
+            $rootScope.oldEnough = $("meta[name='old_enough']").attr('content') === "true";
+
+            // Set new entry dateOfBirth if old enough
+            if ($rootScope.oldEnough) {
+                    $scope.newEntry = {};
+                    $scope.dateOfBirth = '07/28/1986';
+                    this.date = moment().subtract('years', 23);
+            }
 
 
             // Populate date arrays
